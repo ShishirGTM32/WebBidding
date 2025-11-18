1c4c4ccabfc57629c011face3e22432a2eda9b62
Shishir Gautam <119574263+ShishirGTM32@users.noreply.github.com>
2025-11-18 16:50:34 +0545

    Add files via upload

--- bid/settings.py
+++ bid/settings.py
@@ -64,15 +64,19 @@ WSGI_APPLICATION = 'bid.wsgi.application'
 
 AUTH_USER_MODEL = 'bidding.CustomUser'
 
-# Database configuration using environment variables
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='3306'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='require'),
+        },
     }
 }
 
